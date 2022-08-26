579d0a51e2b56a55c4d8ed908cb3d0e35e07b8c9
Veronica Mora <vmora951vmora951@outlook.es>
2022-08-25 20:25:00 -0500

    Examen1

---
+++ VeronicaMora2.js
@@ -0,0 +1,9 @@
+let contador20 = 20;
+
+for (let index = 360; index <=16; index+=20) {
+
+    let resultado = contador20 + index;
+console.log (contador20 + "+" + index + "=" + resultado);
+       
+}
+
